554ed8783bf9c6231d6b907cac297057cdc9c18d
gnzat <tazngmt@gmail.com>
2026-02-07 02:19:58 +0800

    merge

--- src/frontend/api/applicationApi.js
+++ src/frontend/api/applicationApi.js
@@ -10,3 +10,26 @@ export const getApplicationByAppId = async (id) => {
   const res = await axiosClient.get(`/applications/byAppID/${id}`);
   return res.data;
 };
+
+export const submitApplicationApi = async (business_country, business_name, user_id, business_type, email, firstName) => {
+  const res = await axiosClient.post("/", {
+    business_country,
+    business_name,
+    user_id,
+    business_type,
+    email,
+    firstName
+  });
+  return res.data;
+};
+
+/*
+{
+  "business_country": "South Korea",
+  "business_name": "Samsung Enterprise Pte Ltd",
+  "user_id": "00000007",
+  "business_type":"Electronics",
+  "email": "jingjie27@gmail.com",
+  "firstName": "JJ"
+}
+*/
--- src/frontend/api/axiosClient.js
+++ src/frontend/api/axiosClient.js
@@ -8,4 +8,27 @@ const axiosClient = axios.create({
   // },
 });
 
+// attach JWT token automatically
+axiosClient.interceptors.request.use((config) => {
+  const token = store.getState().auth.token;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// auto logout if token invalid/expired
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
 
