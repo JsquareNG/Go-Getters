750f3eaf77a29aab5a6a763c15db355f6dd49fe4
clarisechua <clarisechuaen@gmail.com>
2026-03-18 16:16:17 +0800

    cors

--- src/backend/main.py
+++ src/backend/main.py
@@ -60,7 +60,7 @@ async def log_requests(request: Request, call_next):
 #add cors middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","http://localhost:5174"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","http://localhost:5174","http://localhost:5175","http://localhost:5176"],
     # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
